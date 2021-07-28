ac6f7862c6bcc44d785b9803b2ff182f6c92572f
dmitrykuzan <78803281+dmitrykuzan@users.noreply.github.com>
2021-07-28 12:47:43 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper('.main', {
+	loop:true,
+	speed:2000,
+      navigation: {
+        nextEl: '.main__arrow--next',
+        prevEl: '.main__arrow--prev',
+      },
+      pagination: {
+        el: '.main__pagination',
+      },
+    });
+
